3f06ec5dd64b13e5b46f41a716def724bc259432
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-11-20 18:07:58 -0600

    Installing and testing

--- app/page.js
+++ app/page.js
@@ -1,10 +1,19 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter, Text } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      <Card>
+  <CardBody>
+    <Text>View a summary of all your customers over the last month.</Text>
+  </CardBody>
+</Card>
+      <Button fontSize={{ base: "24px", md: "40px", lg: "56px" }}  bgColor={{ base: "yellow", md: "black", lg: "red" }}>Welcome</Button>
+      <Button fontSize={["24px", "40px", "56px"]}  bgColor={{ base: "yellow", md: "black", lg: "red" }}>Welcome</Button>
+      {/* <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.js</code>
@@ -89,7 +98,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   )
 }
