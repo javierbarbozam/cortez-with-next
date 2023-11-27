de59534a2a92d49bd08f7f30a8d7c08ff57d69a9
Heylin-Loaiza <heylinloaiza.s@gmail.com>
2023-11-27 13:57:49 -0600

    Delete and move some files

--- app/page.js
+++ app/page.js
@@ -1,12 +1,7 @@
-import styles from './page.module.css';
-import { Button, Card, CardHeader, CardBody, CardFooter, Text } from '@chakra-ui/react';
-
-
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-
+    <main>
+      <p>hello</p>
     </main>
   )
 }
--- app/theme.js
+++ app/theme.js
@@ -1,10 +1,4 @@
 import { extendTheme } from '@chakra-ui/react'
-import { cardAnatomy } from '@chakra-ui/anatomy'
-import { createMultiStyleConfigHelpers } from '@chakra-ui/react'
-
-const { definePartsStyle } =
-  createMultiStyleConfigHelpers(cardAnatomy.keys)
-
 
 export const theme = extendTheme({
   colors: {
