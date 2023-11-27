12e566c17a0fcb386ee8163eab988f0805a61a16
Heylin-Loaiza <heylinloaiza.s@gmail.com>
2023-11-27 12:08:18 -0600

    delete default files

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import { Card, CardHeader, CardBody, CardFooter, Text } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <Card>
   <CardBody>
     <Text>View a summary of all your customers over the last month.</Text>
