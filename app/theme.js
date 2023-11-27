1bb171852ad76344fefa2f16e155c73263e6d4c2
Heylin-Loaiza <heylinloaiza.s@gmail.com>
2023-11-27 12:55:03 -0600

    remove unnecessary code

--- app/theme.js
+++ app/theme.js
@@ -1,10 +1,4 @@
 import { extendTheme } from '@chakra-ui/react'
-import { cardAnatomy } from '@chakra-ui/anatomy'
-import { createMultiStyleConfigHelpers } from '@chakra-ui/react'
-
-const { definePartsStyle } =
-  createMultiStyleConfigHelpers(cardAnatomy.keys)
-
 
 export const theme = extendTheme({
   colors: {
@@ -16,18 +10,6 @@ export const theme = extendTheme({
     }
   },
   components: {
-    Card: {
-      baseStyle: {
-        
-      },
-      variants: {
-        elevated: definePartsStyle({
-          container: {
-            boxShadow: '0px 5px 12px 0px rgba(51, 67, 70, 0.10), 0px 22px 22px 0px rgba(51, 67, 70, 0.09), 0px 49px 29px 0px rgba(51, 67, 70, 0.05), 0px 87px 35px 0px rgba(51, 67, 70, 0.01), 0px 135px 38px 0px rgba(51, 67, 70, 0.00)'
-          }
-        })
-      }
-    },
     Button: {
       baseStyle: {
         fontWeight: 'bold', // Normally, it is "semibold"
