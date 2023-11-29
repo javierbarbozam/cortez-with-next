b5986795d1cbaf912880a822892728a2c414641d
javierbarbozam <barboza.javier497@gmail.com>
2023-11-28 20:14:54 -0600

    Add title page state

--- app/metodologia/page.js
+++ app/metodologia/page.js
@@ -1,28 +1,48 @@
-import { Heading, Link } from "@chakra-ui/react";
+"use client";
+
 import NextLink from "next/link";
-import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/react";
+import {
+  Heading,
+  Link,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
+import { useState } from "react";
 import { Doman } from "./components/doman";
 import { Reality } from "./components/reality";
 
+const title = (current) => {
+  const data = {
+    doman: 'Explora nuestro método de enseñanza: Aprende sobre el Método Doman',
+    reality: 'Lectura que Cobre Vida: Descubre la Realidad Aumentada',
+  }
+  return data[current]
+};
+
 export default function Methodology() {
+  const [currentTab, setCurrentTab] = useState('reality')
+
+  const titleChange = () => {
+    currentTab === 'reality' ? setCurrentTab('doman') : setCurrentTab('reality')
+  }
+
   return (
     <main>
-      <Heading>Metodologia</Heading>
       <Link as={NextLink} href="/">
         Home
       </Link>
-      <Tabs>
+      <Heading>{title(currentTab)}</Heading>
+      <Tabs onChange={() => {titleChange()}}>
         <TabList>
           <Tab>Realidad Aumentada</Tab>
           <Tab>Método Doman</Tab>
         </TabList>
         <TabPanels>
-          <TabPanel>
-            {Reality()}
-          </TabPanel>
-          <TabPanel>
-            {Doman()}
-          </TabPanel>
+          <TabPanel>{Reality()}</TabPanel>
+          <TabPanel>{Doman()}</TabPanel>
         </TabPanels>
       </Tabs>
     </main>
