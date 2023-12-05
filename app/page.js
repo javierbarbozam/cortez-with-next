e4567e3814850fca948bdfb401564b4344fd34dc
javierbarbozam <barboza.javier497@gmail.com>
2023-12-05 13:28:17 -0600

    Delete no needed components

--- app/page.js
+++ app/page.js
@@ -10,13 +10,6 @@ import AboutBookCard from './components/AboutBookCard'
 export default function Home() {
   return (
     <main>
-      <Card>
-        <CardBody>
-          <Text>View a summary of all your customers over the last month.</Text>
-        </CardBody>
-      </Card>
-      <Button fontSize={{ base: "24px", md: "40px", lg: "56px" }}  bgColor={{ base: "yellow", md: "black", lg: "red" }}>Welcome</Button>
-      <Button fontSize={["24px", "40px", "56px"]}  bgColor={{ base: "yellow", md: "black", lg: "red" }}>Welcome</Button>
       <HomeCard/>
       <CardImgLeft/>
       <CardApp/>
