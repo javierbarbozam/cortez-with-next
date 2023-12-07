a753732ac7c10d7435f3321234c25461bcab2c41
javierbarbozam <barboza.javier497@gmail.com>
2023-12-07 16:46:02 -0600

    Add Card data to config file
    This was made in order to make every card component dynamic, without any hard coded information

--- app/utils/config.js
+++ app/utils/config.js
@@ -13,10 +13,10 @@ export const books = [
 
 export const methodologyTitles = (current) => {
   const data = {
-    doman: 'Explora nuestro método de enseñanza: Aprende sobre el Método Doman',
-    reality: 'Lectura que cobra vida: Descubre la Realidad Aumentada',
-  }
-  return data[current]
+    doman: "Explora nuestro método de enseñanza: Aprende sobre el Método Doman",
+    reality: "Lectura que cobra vida: Descubre la Realidad Aumentada",
+  };
+  return data[current];
 };
 
 export const appNavigation = [
@@ -28,7 +28,17 @@ export const appNavigation = [
 ];
 
 export const contactData = [
-  { title: 'instagram', link: 'https://www.instagram.com/cortezamarillo_cr/' },
-  { title: 'phone', link: 'tel:88381532' },
-  { title: 'email', link: 'mailto:hola@cortezamarillo-cr.com' }
+  { title: "instagram", link: "https://www.instagram.com/cortezamarillo_cr/" },
+  { title: "phone", link: "tel:88381532" },
+  { title: "email", link: "mailto:hola@cortezamarillo-cr.com" },
 ];
+
+export const pagesData = {
+  home: {
+    about: {
+      title: "¿Quiénes somos?",
+      description:
+        "CortezAmarillo es una empresa costarricense de innovación en el ámbito de la educación, específicamente para brindar alternativas de lectura en etapas tempranas de aprendizaje, que combine lectura tradicional con herramientas tecnológicas para lograr un vínculo emocional y una experiencia más placentera.",
+    },
+  },
+};
