600548486791eff1d99b0cb95c279e67d4d806ba
Vale <valeriams13@gmail.com>
2023-12-06 20:25:56 -0600

    Update book banner order

--- app/page.js
+++ app/page.js
@@ -11,13 +11,13 @@ import BookBanner from "./components/BookBanner";
 export default function Home() {
   return (
     <main>
+      <BookBanner/>
       <HomeCard/>
       <CardImgLeft/>
       <CardApp/>
       <MissionCard/>
       <GoalsCard/>
       <AboutBookCard/>
-      <BookBanner/>
     </main>
   );
 }
