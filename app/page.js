96e355a0cbb5095ab9ed5b89cb575028457f57db
javierbarbozam <barboza.javier497@gmail.com>
2023-12-07 16:49:54 -0600

    Add Card to home page
    Description Card added to home page, with the new theming features, and props for dynamic data to be parsed.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import BookBanner from "./components/BookBanner";
+import { DescriptionCard } from "./components/Card/DescriptionCard";
+import { pagesData } from "./utils/config";
+
+const {about} = pagesData.home;
 
 export default function Home() {
   return (
     <main>
-      <BookBanner/>
+      <BookBanner />
+      <DescriptionCard title={about.title} description={about.description}/>
     </main>
   );
 }
