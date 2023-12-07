18845e8f56481b72a1106a56ddae204bd83f3c0b
javierbarbozam <barboza.javier497@gmail.com>
2023-12-06 21:52:24 -0600

    Revert "Revert "Change book banner""
    This reverts commit 7883a9027ad6d6eba5bdc10543bcba6fb2e81e8d.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
 import BookBanner from "./components/BookBanner";
+import HomeCard from "./components/HomeCard";
+import CardImgLeft from "./components/CardImgLeft";
+import CardApp from "./components/CardApp";
+import MissionCard from "./components/MissionCard";
+import GoalsCard from "./components/GoalsCard";
+import AboutBookCard from "./components/AboutBookCard";
 
 export default function Home() {
   return (
     <main>
       <BookBanner/>
+      <HomeCard/>
+      <CardImgLeft/>
+      <CardApp/>
+      <MissionCard/>
+      <GoalsCard/>
+      <AboutBookCard/>
     </main>
   );
 }
