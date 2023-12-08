10edb55911c4cf59154d0f833f519e4feac9ada3
javierbarbozam <barboza.javier497@gmail.com>
2023-12-08 16:12:11 -0600

    Add media Cards
    There was two vertical media cards pending, so now the data and components were added.

--- app/nosotros/page.js
+++ app/nosotros/page.js
@@ -1,22 +1,39 @@
 import { MediaCard } from "../components/Card/MediaCard";
 import { DescriptionCard } from "../components/Card/DescriptionCard";
 import { pagesData } from "../utils/config";
+import { Container, Flex } from "@chakra-ui/react";
 
-const { history, goals } = pagesData.about;
+const { history, goals, mission, vision } = pagesData.about;
 
 export default function AboutUs() {
   return (
     <main>
-      <DescriptionCard
-        title={history.title}
-        description={history.description}
-      />
-      <MediaCard
-        direction="row-reverse"
-        media={goals.media}
-        title={goals.title}
-        info={goals.description}
-      />
+      <Container maxW={{md:'1000px'}} display='grid' gap='60px' m='60px auto'>
+        <DescriptionCard
+          title={history.title}
+          description={history.description}
+        />
+        <MediaCard
+          direction="row-reverse"
+          media={goals.media}
+          title={goals.title}
+          info={goals.description}
+        />
+        <Flex flexDir={{ base: "column", sm: "row" }} gap="6">
+          <MediaCard
+            direction="column"
+            media={mission.media}
+            title={mission.title}
+            info={mission.description}
+          />
+          <MediaCard
+            direction="column"
+            media={goals.media}
+            title={vision.title}
+            info={vision.description}
+          />
+        </Flex>
+      </Container>
     </main>
   );
 }
