f340db37734db09b2b552c358625bb31784b7211
javierbarbozam <barboza.javier497@gmail.com>
2023-12-08 01:35:17 -0600

    Add method card to home page

--- app/page.js
+++ app/page.js
@@ -1,14 +1,21 @@
 import BookBanner from "./components/BookBanner";
 import { DescriptionCard } from "./components/Card/DescriptionCard";
+import { MediaCard } from "./components/Card/MediaCard";
 import { pagesData } from "./utils/config";
 
-const {about} = pagesData.home;
+const { about, learningMethod } = pagesData.home;
 
 export default function Home() {
   return (
     <main>
       <BookBanner />
-      <DescriptionCard title={about.title} description={about.description}/>
+      <DescriptionCard title={about.title} description={about.description} />
+      <MediaCard
+        media={learningMethod.media}
+        title={learningMethod.title}
+        info={learningMethod.description}
+        cta={learningMethod.cta}
+      />
     </main>
   );
 }
