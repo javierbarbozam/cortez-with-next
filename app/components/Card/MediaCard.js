ce2ebdf11cc1e30b3697e731089ae224e533883a
javierbarbozam <barboza.javier497@gmail.com>
2023-12-08 16:11:21 -0600

    Create Vertical MediaCard
    In order to use the same MediaCard component, a new structure was made, so now, direction prop return a horizontal or vertical component depending on the input received (column, row or row-reverse)

--- app/components/Card/MediaCard.js
+++ app/components/Card/MediaCard.js
@@ -9,38 +9,74 @@ import {
   CardFooter,
 } from "@chakra-ui/react";
 
-export const MediaCard = ({ direction = "row", media, title, info, cta }) => {
-  return (
-    <Card
-      variant="mediaCard"
-      direction={{ base: "column", sm: direction }}
-      h={{ sm: "300px", md: "350px" }}
+const RowCard = ({ direction, media, title, info, cta }) => (
+  <Card
+    variant="mediaCard"
+    direction={{ base: "column", sm: direction }}
+    h={{ sm: "300px", md: "350px" }}
+  >
+    <Box height={["50%", "100%"]} width={["100%", "50%"]}>
+      {media}
+    </Box>
+    <Box
+      mt={{ base: "15px", sm: "0" }}
+      width={["100%", "50%"]}
+      display="flex"
+      gap="4"
+      flexDir="column"
+      alignItems="space-between"
     >
-      <Box height={["50%", "100%"]} width={["100%", "50%"]}>
-        {media}
-      </Box>
-      <Box
-        mt={{base: '15px', sm: '0'}}
-        width={["100%", "50%"]}
-        display="flex"
-        gap="4"
-        flexDir="column"
-        alignItems="space-between"
-      >
-        <CardBody>
-          <CardHeader>
-            <Heading variant="mediaCard" size={["md", "md", "lg"]}>
-              {title}
-            </Heading>
-          </CardHeader>
-          <Text fontSize={["md", "md", "xl"]}>{info}</Text>
-        </CardBody>
+      <CardBody>
+        <CardHeader>
+          <Heading variant="mediaCard" size={["md", "md", "lg"]}>
+            {title}
+          </Heading>
+        </CardHeader>
+        <Text fontSize={["md", "md", "xl"]}>{info}</Text>
+      </CardBody>
+      <CardFooter>
+        {cta
+          ? cta.map((element, index) => <Flex key={index}>{element}</Flex>)
+          : null}
+      </CardFooter>
+    </Box>
+  </Card>
+);
+
+const ColumnCard = ({ media, title, info, cta }) => (
+  <Card variant="columnCard">
+    <Box width="100%" height={{base: '150px', md: '250px'}}>
+      {media}
+    </Box>
+    <Box display="flex" gap="4" flexDir="column" alignItems="space-between">
+      <CardBody>
+        <CardHeader>
+          <Heading variant="mediaCard" size={["md", "md", "lg"]}>
+            {title}
+          </Heading>
+        </CardHeader>
+        <Text fontSize={["md", "md", "xl"]}>{info}</Text>
+      </CardBody>
+      {cta ? (
         <CardFooter>
-          {cta
-            ? cta.map((element, index) => <Flex key={index}>{element}</Flex>)
-            : null}
+          {cta.map((index, element) => (
+            <Flex key={index}>{element}</Flex>
+          ))}
         </CardFooter>
-      </Box>
-    </Card>
+      ) : null}
+    </Box>
+  </Card>
+);
+
+export const MediaCard = ({ direction = "row", media, title, info, cta }) =>
+  direction !== "column" ? (
+    <RowCard
+      direction={direction}
+      media={media}
+      title={title}
+      info={info}
+      cta={cta}
+    />
+  ) : (
+    <ColumnCard media={media} title={title} info={info} cta={cta} />
   );
-};
