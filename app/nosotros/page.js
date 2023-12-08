debb780d30cb51e20b6ab346ba73c2a2a1356872
javierbarbozam <barboza.javier497@gmail.com>
2023-12-08 02:15:53 -0600

    Update page.js

--- app/nosotros/page.js
+++ app/nosotros/page.js
@@ -12,6 +12,7 @@ export default function AboutUs() {
         description={history.description}
       />
       <MediaCard
+        direction="row-reverse"
         media={goals.media}
         title={goals.title}
         info={goals.description}
