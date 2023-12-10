76f7a94127be4d1e5cbdeadde6db67e41d51ecaa
javierbarbozam <barboza.javier497@gmail.com>
2023-12-09 23:51:48 -0600

    prettier applied

--- app/metodologia/page.js
+++ app/metodologia/page.js
@@ -15,15 +15,15 @@ import { pagesData } from "../utils/config";
 import { DescriptionCard } from "../components/Card/DescriptionCard";
 import { AppCard } from "../components/Card/AppCard";
 
-const {augmented_reality, doman} = pagesData.methodology
-const augmentedTitle = augmented_reality.title
-const domanTitle = doman.title
+const { augmented_reality, doman } = pagesData.methodology;
+const augmentedTitle = augmented_reality.title;
+const domanTitle = doman.title;
 
 const Reality = () => {
   return (
     <>
-      <DescriptionCard description={augmented_reality.description}/>
-      <AspectRatio maxW="560px" ratio={16/9}>
+      <DescriptionCard description={augmented_reality.description} />
+      <AspectRatio maxW="560px" ratio={16 / 9}>
         <iframe
           title="naruto"
           src="https://www.youtube.com/embed/QhBnZ6NPOY0"
@@ -37,31 +37,31 @@ const Reality = () => {
 
 const Doman = () => (
   <>
-    <DescriptionCard description={doman.description[0]}/>
-    INSERT IMAGE
-    INSERT IMAGE
-    <DescriptionCard description={doman.description[1]}/>
+    <DescriptionCard description={doman.description[0]} />
+    INSERT IMAGE INSERT IMAGE
+    <DescriptionCard description={doman.description[1]} />
   </>
-  
-)
-
-
-
-
-
+);
 
 export default function Methodology() {
-  const [currentTab, setCurrentTab] = useState(augmentedTitle)
+  const [currentTab, setCurrentTab] = useState(augmentedTitle);
 
   const titleChange = () => {
-    currentTab === augmentedTitle ? setCurrentTab(domanTitle) : setCurrentTab(augmentedTitle)
-  }
+    currentTab === augmentedTitle
+      ? setCurrentTab(domanTitle)
+      : setCurrentTab(augmentedTitle);
+  };
 
   return (
     <main>
       <Container centerContent>
         <Heading>{currentTab}</Heading>
-        <Tabs variant='content' onChange={() => {titleChange()}}>
+        <Tabs
+          variant="content"
+          onChange={() => {
+            titleChange();
+          }}
+        >
           <TabList>
             <Tab>Realidad Aumentada</Tab>
             <Tab>Método Doman</Tab>
