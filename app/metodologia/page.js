f814882543d011654d7fbe8729c2dc11d1b660fd
javierbarbozam <barboza.javier497@gmail.com>
2023-12-09 23:47:27 -0600

    Add Description Card
    The description card was added to the page in order to keep familiarity with the other pages. Also, the previous import with data were deleted, and all the info comes from the same object.

--- app/metodologia/page.js
+++ app/metodologia/page.js
@@ -1,33 +1,67 @@
 "use client";
 
-import NextLink from "next/link";
 import {
   Heading,
-  Link,
   Tabs,
   TabList,
   Tab,
   TabPanels,
   TabPanel,
   Container,
+  AspectRatio,
 } from "@chakra-ui/react";
 import { useState } from "react";
-import { Doman } from "./components/doman";
-import { Reality } from "./components/reality";
-import { methodologyTitles } from "../utils/config";
+import { pagesData } from "../utils/config";
+import { DescriptionCard } from "../components/Card/DescriptionCard";
+import { AppCard } from "../components/Card/AppCard";
+
+const {augmented_reality, doman} = pagesData.methodology
+const augmentedTitle = augmented_reality.title
+const domanTitle = doman.title
+
+const Reality = () => {
+  return (
+    <>
+      <DescriptionCard description={augmented_reality.description}/>
+      <AspectRatio maxW="560px" ratio={16/9}>
+        <iframe
+          title="naruto"
+          src="https://www.youtube.com/embed/QhBnZ6NPOY0"
+          allowFullScreen
+        />
+      </AspectRatio>
+      <AppCard />
+    </>
+  );
+};
+
+const Doman = () => (
+  <>
+    <DescriptionCard description={doman.description[0]}/>
+    INSERT IMAGE
+    INSERT IMAGE
+    <DescriptionCard description={doman.description[1]}/>
+  </>
+  
+)
+
+
+
+
+
 
 export default function Methodology() {
-  const [currentTab, setCurrentTab] = useState('reality')
+  const [currentTab, setCurrentTab] = useState(augmentedTitle)
 
   const titleChange = () => {
-    currentTab === 'reality' ? setCurrentTab('doman') : setCurrentTab('reality')
+    currentTab === augmentedTitle ? setCurrentTab(domanTitle) : setCurrentTab(augmentedTitle)
   }
 
   return (
     <main>
       <Container centerContent>
-        <Heading>{methodologyTitles(currentTab)}</Heading>
-        <Tabs variant='content' isLazy onChange={() => {titleChange()}}>
+        <Heading>{currentTab}</Heading>
+        <Tabs variant='content' onChange={() => {titleChange()}}>
           <TabList>
             <Tab>Realidad Aumentada</Tab>
             <Tab>Método Doman</Tab>
