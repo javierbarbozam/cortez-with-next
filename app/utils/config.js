88df02206cc288f0f39bd9bb063615aa960b4337
javierbarbozam <barboza.javier497@gmail.com>
2023-12-12 11:27:54 -0600

    Add doman images to config file

--- app/utils/config.js
+++ app/utils/config.js
@@ -6,6 +6,8 @@ import objectivesImg from "../../public/images/objectives.webp";
 import appImg from "../../public/images/download-app.webp";
 import visionImg from "../../public/images/vision-img.webp";
 import missionImg from "../../public/images/mission-img.webp";
+import domanFirstImg from "../../public/images/doman-first-img.webp";
+import domanSecondImg from "../../public/images/doman-second-img.webp";
 import nosConocemosBook from "../../public/images/book-crAnimals.png";
 import { AppStoreLink, GooglePlayLink } from "../components/StoreLink";
 
@@ -115,6 +117,7 @@ export const pagesData = {
         "El método Doman, aboga por la estimulación temprana y el desarrollo cognitivo en niños, CortezAmarillo con esto ofrece una experiencia educativa que integra magistralmente la tradición con la innovación. Glenn Doman, pionero en el campo de la educación temprana, propuso un enfoque que destaca la importancia de estimular los sentidos desde una edad temprana para potenciar el aprendizaje. Este principio se refleja en la esencia de CortezAmarillo, donde las páginas no solo están bellamente ilustradas, sino que también se han diseñado con elementos interactivos para una mejor lectura",
         "La mezcla única entre el método Doman y las nuevas ideas en CortezAmarillo no es solo sobre enseñar cosas; está diseñada para hacer que a los niños les encante explorar y aprender. Queremos que descubran más allá de lo que aprenden en la escuela, ayudándolos a crecer de una manera que abarque todo, más allá de las aulas. Cada libro no es solo un libro, es como un amigo que te lleva en un viaje educativo, mostrándote un mundo donde aprender es mágico y descubrir es parte de la realidad.",
       ],
+      media: [<ImageComponent key={0} source={domanFirstImg}/>,<ImageComponent key={1} source={domanSecondImg}/>]
     },
   },
   app: {
