9385849b6ef55210bab0bf1ac796e862be5dfd87
javierbarbozam <barboza.javier497@gmail.com>
2024-01-08 16:39:21 -0600

    Delete test form schema

--- app/components/CreateClient.js
+++ app/components/CreateClient.js
@@ -1,57 +1,5 @@
-"use client";
-
-import React, { useState } from "react";
 import { CheckoutForm } from "./CheckoutForm";
 
 export const CreateClient = ({ createCustomer }) => {
-  const [formData, setFormData] = useState({
-    address: {
-      city: null,
-      line1: null,
-      line2: null,
-      postalCode: null,
-      state: null,
-    },
-    description: null,
-    email: null,
-    name: null,
-    phone: null,
-    shipping: {
-      address: {
-        city: null,
-        line1: null,
-        line2: null,
-        postalCode: null,
-        state: null,
-      },
-      name: "",
-      phone: null,
-    },
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    const keys = name.split(".");
-    let data = formData;
-
-    keys.reduce((acc, key, index) => {
-      if (index === keys.length - 1) {
-        acc[key] = value;
-      } else {
-        if (!acc[key]) acc[key] = {};
-        return acc[key];
-      }
-      return null;
-    }, data);
-
-    setFormData({ ...data });
-  };
-
-  return (
-    <CheckoutForm
-      formData={formData}
-      action={createCustomer}
-      handleChange={handleChange}
-    />
-  );
+  return <CheckoutForm action={createCustomer} />;
 };
