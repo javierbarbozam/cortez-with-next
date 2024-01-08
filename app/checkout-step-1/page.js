770c99ab28cd4a271b3d3ab4def2775be95a1f7d
javierbarbozam <barboza.javier497@gmail.com>
2024-01-08 00:56:08 -0600

    Fix onvo schema and redirect when failing request

--- app/checkout-step-1/page.js
+++ app/checkout-step-1/page.js
@@ -1,53 +1,61 @@
-import { redirect } from 'next/navigation';
-import { CreateClient } from '../components/CreateClient';
+import { redirect } from "next/navigation";
+import { CreateClient } from "../components/CreateClient";
 
-export default async function CheckoutStep1({ searchParams }) {
-
+export default async function CheckoutStep1() {
   async function createCustomer(formData) {
-    'use server';
+    "use server";
 
     const rawFormData = {
       address: {
-        city: formData.get('address.city'),
-        country: formData.get('address.country'),
-        line1: formData.get('address.line1'),
-        line2: formData.get('address.line2'),
-        postalCode: formData.get('address.postalCode'),
-        state: formData.get('address.state'),
+        city: formData.get("address.city"),
+        country: "CR",
+        line1: formData.get("address.line1"),
+        line2: formData.get("address.line2"),
+        postalCode: formData.get("address.postalCode"),
+        state: formData.get("address.state"),
       },
-      description: formData.get('description'),
-      email: formData.get('email'),
-      name: formData.get('name'),
-      phone: formData.get('phone'),
+      description: formData.get("description"),
+      email: formData.get("email") || null,
+      name: formData.get("name"),
+      phone: formData.get("phone") ? "+506" + formData.get("phone") : null,
       shipping: {
         address: {
-          city: formData.get('shipping.address.city'),
-          country: formData.get('shipping.address.country'),
-          state: formData.get('shipping.address.state'),
-          line1: formData.get('shipping.address.line1'),
-          line2: formData.get('shipping.address.line2'),
-          postalCode: formData.get('shipping.address.postalCode'),
+          city: formData.get("shipping.address.city"),
+          country: "CR",
+          state: formData.get("shipping.address.state"),
+          line1: formData.get("shipping.address.line1"),
+          line2: formData.get("shipping.address.line2"),
+          postalCode: formData.get("shipping.address.postalCode"),
         },
-        name: formData.get('shipping.name'),
-        phone: formData.get('shipping.phone'),
+        name: formData.get("shipping.name")
+          ? formData.get("shipping.name")
+          : formData.get("name"),
+        phone: formData.get("shipping.phone")
+          ? "+506" + formData.get("shipping.phone")
+          : null,
       },
     };
 
-    const { id } = await fetch('https://api.onvopay.com/v1/customers', {
-      method: 'POST',
+    const clientData = await fetch("https://api.onvopay.com/v1/customers", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_ONVO_TOKEN_SECRET}`,
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_ONVO_TOKEN_SECRET}`,
       },
       body: JSON.stringify(rawFormData),
     }).then((res) => res.json());
 
-    redirect(`/checkout-step-2?customerId=${id}`);
+    if (clientData.id) {
+      redirect(
+        `/checkout-step-2?customerId=${clientData.id}&description=${clientData.description}`
+      );
+    } else {
+      redirect("/checkout-error");
+    }
   }
 
   return (
     <>
-      <h1>Checkout 1</h1>
       <CreateClient createCustomer={createCustomer} />
     </>
   );
